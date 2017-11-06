fb0c44109f585784c64a6ae56791c1ca840f859d
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-06 17:31:27 +0100

    First Iteration

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,26 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction  = 'up';
+    this.interval;
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    this.interval = setInterval(() => this.update(),1000);
+  }
+  stop() {
+    clearInterval(this.interval);
+  }
+  update() {
+    this.log();
+  }
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);}
 }
 
 module.exports = Elevator;
