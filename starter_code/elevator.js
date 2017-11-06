833285a89924e0c60f1e8cc7e45362415263aab3
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-06 23:20:25 +0100

    added color

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,35 +1,49 @@
+let chalk = require('chalk');
+
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = []; // Have floor to go
-    this.direction  = 'up';
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = []; // Have floor to go
+    this.direction = 'up';
     this.interval;
     this.waitingList = []; // Waiting for elevator in floor
     this.passengers = []; // Currently in elevator
   }
 
   start() {
-    this.interval = setInterval(() => this.update(),1000);
+    this.interval = setInterval(() => this.update(), 1000);
   }
 
   stop() {
     clearInterval(this.interval);
   }
 
+  floorUp() {
+    if ((this.floor + 1) < this.MAXFLOOR) {
+      this.floor += 1;
+    } else console.log("IF YOU GO UP WILL LAUNCH IN ROCKET!");
+  }
+
+  floorDown() {
+    if (this.floor - 1 < 0) console.log("WANT TO BE A MOLE?");
+    else {
+      this.floor -= 1;
+    }
+  }
+
   update() {
-    this.log();
-    if (this.requests.length!=0){
-      if (this.floor < this.requests[0]){
+    if (this.requests.length != 0) {
+      if (this.floor < this.requests[0]) {
+        this.direction = "up";
         this.floorUp();
-      } else {
+      } else if (this.floor > this.requests[0]) {
+        this.direction = "down";
         this.floorDown();
       }
+      this.enterAndExit();
     }
-    if (this.requests[0] == this.floor){
-      this._passengersEnter();
-      this._passengersLeave();
-    }
+    this.log();
     console.log(this.requests);
     console.log(this.waitingList);
     console.log(this.passengers);
@@ -37,60 +51,67 @@ class Elevator {
 
   _passengersEnter() {
     // Entra en ascensor y marca el floor al que quiere ir (PRIORITARIO).
-    console.log(`Welcome to floor ${this.floor}`);
     this.waitingList.forEach((person) => {
-      if (person.originFloor == this.floor){
-      this.requests.shift();
-      // Insert in request floor where go with preference over the other.
-      this.requests.sort((a,b) => a-b).unshift(person.destinationFloor);
-      // LOG
-      console.log(`${person.name} has enter the elevator`);
-      // Insert in elevator and remove from waitingList
-      this.passengers.push(person);
+      if (person.originFloor == this.floor) {
+        // Insert in request floor where go with preference over the other.
+        this.requests.push(person.destinationFloor);
+        // LOG
+        console.log(chalk.green(`${person.name} has enter the elevator`));
+        // Insert in elevator
+        this.passengers.push(person);
       }
     });
-    // Remove all mounted passengers:
+    // Remove all mounted passengers from waitingList:
     this.waitingList = this.waitingList.filter((obj) => {
       return obj.originFloor !== this.floor;
     });
   };
 
   _passengersLeave() {
-    // Sale del ascensor y se saca de requests.
+    // Exit from elevator and remove from passengers.
     this.passengers.forEach((personGoesOut) => {
-      if (personGoesOut.destinationFloor == this.floor){
-        console.log(`Bye ${personGoesOut.name} in floor ${this.floor}`);
+      if (personGoesOut.destinationFloor == this.floor) {
+        console.log(chalk.red(`Bye ${personGoesOut.name} in floor ${this.floor}`));
         // Go out from elevator
-        this.requests.shift();
-    }
-  });
-  // Remove all arrive passengers
-  this.passengers = this.passengers.filter((obj) => {
-    return obj.destinationFloor !== this.floor;
-  });
-};
+      }
+    });
+    // Remove all passengers in the destination floor
+    this.passengers = this.passengers.filter((obj) => {
+      return obj.destinationFloor !== this.floor;
+    });
+    this._removeRequestsRepeatedAndArrived();
+  };
 
-  floorUp() {
-    if ((this.floor + 1) < this.MAXFLOOR){
-      this.floor += 1;
-    }
-    else console.log("IF YOU GO UP WILL LAUNCH THE ROCKET!");
-  }
-
-  floorDown() {
-    if (this.floor - 1 < 0) console.log("WANT TO BE A MOLE?");
-    else{
-      this.floor -= 1;
-    }
+  _removeRequestsRepeatedAndArrived() {
+    // Remove duplicate values
+    this.requests = this.getUniqueValues(this.requests);
+    // Remove same floor (arrive)
+    this.requests = this.requests.filter((obj) => {
+      return obj !== this.floor;
+    });
   }
 
   call(person) { // Call, insert request of originFloor and stay waitingList.
     this.requests.push(person.originFloor);
+    // this.requests = this.getUniqueValues(this.requests);
     this.waitingList.push(person);
   }
 
   log() {
-    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);}
+    console.log(chalk.yellow(`Direction: ${this.direction} | Floor: ${this.floor}`));
+  }
+
+  getUniqueValues(array) {
+    var unique = array.filter((elem, index, self) => {
+      return index == self.indexOf(elem);
+    });
+    return unique;
+  }
+
+  enterAndExit() {
+    this._passengersEnter();
+    this._passengersLeave();
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -4,14 +4,30 @@ const Person = require('./person.js');
 let elevator = new Elevator();
 elevator.update();
 elevator.start();
-elevator.floorUp();
-elevator.floorDown();
-elevator.floorDown();
-
 
 let person1 = new Person("pepe", 0, 5);
+let person5 = new Person("pepe", 0, 8);
 let person2 = new Person("maria", 2, 0);
 let person3 = new Person("jose",3,1);
+let person4 = new Person("anciano",3,2);
 elevator.call(person1);
 elevator.call(person2);
 elevator.call(person3);
+elevator.call(person4);
+elevator.call(person5);
+
+setInterval(() => {
+ elevator.call(new Person(makeid(), Math.round(Math.random() * 9), Math.round(Math.random() * 9)));
+},5000);
+
+function makeid() {
+  let text = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+console.log(makeid());
